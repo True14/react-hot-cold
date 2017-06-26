b100734996c23714eb5bb2a91246cf20a54ee6ce
Kyle Rogers <kylerogers334@gmail.com>
2017-06-26 20:31:55 +0000

    Add redux to our add

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
 import './reset.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
 
 import './reset.css';
 import './index.css';
 
 import Game from './components/game';
 
+//REDUX
+import {newGame} from './actions/newGame';
+import {toggleModal} from './actions/toggleModal';
+import {makeGuess} from './actions/makeGuess';
+import reducer from './reducers/index.js';
+
+const store = createStore(reducer);
+console.log(store.getState());
+store.dispatch(makeGuess(5));
+console.log(store.getState());
+
 ReactDOM.render(
     <Game />,
     document.getElementById('root')
 );
+
+
+
+
+
+
+
---
+++ src/reducers/index.js
@@ -0,0 +1,23 @@
+import {NEW_GAME} from '../actions/newGame';
+import {TOGGLE_MODAL} from '../actions/toggleModal';
+import {MAKE_GUESS} from '../actions/makeGuess';
+
+import {combineReducers} from 'redux';
+
+const initialState = {
+    guesses: [],
+    feedback: 'Make your guess!',
+    correctAnswer: Math.floor(Math.random() * 100) + 1,
+    showInfoModal: false
+};
+
+export default (state=initialState, action) => {
+    if (action.type === NEW_GAME) {
+        return Object.assign({}, initialState, {correctAnswer: Math.floor(Math.random() * 100) + 1});
+    } else if (action.type === TOGGLE_MODAL) {
+        return Object.assign({}, state, {showInfoModal: !state.showInfoModal});
+    } else if (action.type === MAKE_GUESS) {
+        return Object.assign({}, state, {guesses: [...state.guess, action.guess]})
+    }
+    return state;
+};
